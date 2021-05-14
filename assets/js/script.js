cd6eb6341ae2aeb6b0801aa9ba591a7fa210dd1b
Megan Steblay <megan@steblay.net>
2021-05-14 13:55:42 -0500

    Adding storage to LocalStorage and updating files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,60 @@
+/****************************************************************
+* Program: Work Day Scheduler
+*
+* Author: Megan Steblay
+*
+****************************************************************/
+
+
+// VARIABLES 
 let timeDisplayEl = $('#currentDay');
 let buttonEl = $('button[type="button"]');
 let formEl = $('.form-control')
 
 
-// TIME
-
+// TIME (using Luxon)
 let DateTime = luxon.DateTime;
 
-// Current Date and Time at the Top of the Page: 
-let now = DateTime.now();
-
+// Current Date and Time at the top of the page: 
+let now = DateTime.local();
 timeDisplayEl.text(now.toFormat("EEEE','  LLLL L',' t"));
 
-
-// Change colors of inputs using Luxon
+// Variables for time:
 let currentTime = now.toFormat("H");
 let startHour = 9
 
-
-$( ".form-control" ).each(function( index ) {
-
-    if((index + startHour) == (currentTime)) {
+// Function for setting the schedule colors and calling the stored text:
+$(".form-control").each(function (index) {
+    if ((index + startHour) == (currentTime)) {
         $(this).addClass('present')
-    } else if((index + startHour) < (currentTime)) {
+    } else if ((index + startHour) < (currentTime)) {
         $(this).addClass('past')
     } else {
         $(this).addClass('future')
     }
+    // Grabbing the input from storage: 
+    let name = $(this).attr('aria-label');
+    let retrievedValue = JSON.parse(localStorage.getItem(name));
+    $(this).val(retrievedValue);
 });
 
-
-
-//Buttons
-// make the buttons save to Local Storage
-
-function handleButton(event) {
+// Function for saving the input to LocalStorage: 
+function saveButton(event) {
     event.preventDefault();
-
-    console.log(event.target);
-
-    localStorage.setItem("userNotes", $(".form-control").val());
-    
-    let storedItem = localStorage.getItem("userNotes")
-
+    // Get specific button and its corresponding aria-label and input: 
     let btnClicked = $(event.target);
-    //btnClicked.
+    let name = btnClicked.parent().parent().siblings().eq(1).attr('aria-label');
+    let input = btnClicked.parent().parent().siblings().eq(1).val();
+    // Store these in LocalStorage: 
+    localStorage.setItem(name, JSON.stringify(input));
 };
 
-  //Listener
 
- buttonEl.on('click', handleButton);
+// CODE TO TEST DIFFERENT TIMES: 
+// let testTime = 11; // Setting hour with 24-hour clock
+// let hoursToSubtract = currentHour - testTime; 
+// currentHour -= hoursToSubtract; 
+
+
+// Click event listeners for buttons: 
+buttonEl.on('click', saveButton);
